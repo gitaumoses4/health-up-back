2fd0e8e1341a11274d2109a10b276d345731bee8
Moses Gitau <gitaumoses4@gmai.com>
2019-06-26 06:24:01 +0300

    Add profile model

---
+++ src/database/migrations/20190625180327-create-profile.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Profiles', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    fullName: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    medicalFileNumber: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Profiles')
+};
---
+++ src/database/models/profile.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Profile = sequelize.define('Profile', {
+    name: DataTypes.STRING
+  }, {});
+  Profile.associate = function(models) {
+    // associations can be defined here
+  };
+  return Profile;
+};
